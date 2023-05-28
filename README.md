66d7582ee7d745fcb2a2eb989c96600977560ae9
SodaKarate <faragocsaba5@gmail.com>
2023-05-28 15:08:02 +0000

    harmadik feladat kezdes

--- README.md
+++ README.md
@@ -1,17 +1,17 @@
 <h1>Hackaton 2023</h1>
 <h2>Ezt a repot eredményhirdetés után kb egy héttel le fogom venni (ha nem felejtem el)</h2>
                  
-Hardware:                                                                     
+<h2>Hardware:</h2>                                                                     
     Ryzen 7 2700X
     16GB DDR4 RAM
     RX570 8GB
 
-Software:
+<h2>Software:</h2>
     IDE: Visual Studio Code
     Shell: fish 3.6.1
     Operating System: Arch Linux x86-64 (btw)
 
-Resources:
+<h2>Resources:</h2>
     Dobble:
         https://www.youtube.com/watch?v=VTDKqW_GLkw
         Andros barátom
--- pizza/main.py
+++ pizza/main.py
@@ -1,9 +1,30 @@
 import PySimpleGUI as sg
 
+def showCurrentList(adatok):
+    indexek = ['sajtos pizza', 'sonkás pizza', 'gombás pizza', 'hawaii pizza', 'pepsi', 'coca cola', 'viz']
+    layout = []
+
+    for i in range(len(adatok)):
+        if adatok[i]!=0:
+            szoveg=indexek[i],adatok[i]
+            layout.append([sg.Text(szoveg)])
+    layout.append([sg.Button('Vissza')])
+    sg.theme('LightBrown4')
+    window = sg.Window('Pizza rendelés részletek', layout)
+    
+    while True:
+        event, values = window.read()
+        print(event, values)
+        
+        if event == sg.WINDOW_CLOSED or event == 'Vissza':
+            break
+    
+    window.close() 
+
 sg.theme('LightBrown4')
 layout=[[sg.Text('Mit szeretnél rendelni?')],
         [sg.InputText('')],
-        [sg.Button('Oké mehet!'),sg.Button('Mégsem.')]]
+        [sg.Button('Oké mehet!'),sg.Button('Részletek'),sg.Button('Mégsem.')]]
 window=sg.Window('Pizza rendelés',layout)
 
 
@@ -11,6 +32,8 @@ while True:                             # The Event Loop
     event, values = window.read() 
     print(event, values)       
     if event == sg.WIN_CLOSED or event == 'Mégsem.':
-        break      
+        break
+    if event == 'Részletek':
+        showCurrentList([0,1,2,1,0,0,1])
 
 window.close()
